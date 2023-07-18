bac6b6615768eb085b9d6b4773b624f3e4c12ace
utkarsh somvanshi <somvanshi2132001@gmail.com>
2023-07-19 02:30:11 +0530

    commit-utkx2-12

---
+++ server/models/HomeModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const HomeModel = new Schema({
+    carousel: [{
+        type: String,
+        required: true
+    }],
+    faculty: {
+        images: [{
+            type: String,
+            required: true
+        }],
+        description: [{
+            type: String,
+            required: true
+        }]
+    },
+    selectedStudents: {
+        images: [{
+            type: String,
+            required: true
+        }],
+        description: [{
+            type: String,
+            required: true
+        }],
+        air: [{
+            type: Number,
+            required: true
+        }],
+        exam: [{
+            type: String,
+            required: true
+        }]
+    },
+    programs: [{
+        images: [{
+            type: String,
+            required: true
+        }],
+        description: [{
+            type: String,
+            required: true
+        }]
+    }]
+});
+
+module.exports = mongoose.model('Home', HomeModel);
---
+++ server/routes/HomeRoute.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const Home = require('../models/HomeModel');
+
+// Set up Multer for handling file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/home/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage });
+
+// Upload home page content
+router.post('/home/upload', upload.fields([
+    { name: 'carousel', maxCount: 4 }, 
+    { name: 'faculty.images', maxCount: 4 }, 
+    { name: 'selectedStudents.images', maxCount: 4 },
+    { name: 'programs.*.images', maxCount: 4 },
+]), async (req, res) => {
+    try {
+        const { carousel, faculty, selectedStudents, programs } = req.files;
+        const { facultyDescription, selectedStudentsDescription, selectedStudentsAIR, selectedStudentsExam, programDescription } = req.body;
+
+        const homeContent = new Home({
+            carousel: carousel.map(file => file.filename),
+            faculty: {
+                images: faculty.map(file => file.filename),
+                description: facultyDescription,
+            },
+            selectedStudents: {
+                images: selectedStudents.map(file => file.filename),
+                description: selectedStudentsDescription,
+                air: selectedStudentsAIR,
+                exam: selectedStudentsExam,
+            },
+            programs: programs.map((program, index) => ({
+                images: program.images.map(file => file.filename),
+                description: programDescription[index],
+            })),
+        });
+
+        await homeContent.save();
+
+        res.json({ message: 'Home page content uploaded successfully', homeContent });
+    } catch (error) {
+        console.error('Error uploading home page content:', error);
+        res.status(500).json({ error: 'An error occurred while uploading home page content' });
+    }
+});
+
+// Fetch home page content
+router.get('/home/content', async (req, res) => {
+    try {
+        const homeContent = await Home.findOne({}); // Assuming there's only one document for the home page content
+        res.json(homeContent);
+    } catch (error) {
+        console.error('Error fetching home page content:', error);
+        res.status(500).json({ error: 'An error occurred while fetching home page content' });
+    }
+});
+
+module.exports = router;
