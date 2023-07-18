fad173ae0b9e88cfc8784d86ead4946e5826d598
utkarsh somvanshi <somvanshi2132001@gmail.com>
2023-07-18 15:35:29 +0530

    commit-utkx2-7

--- server/routes/ContentRoute.js
+++ server/routes/ContentRoute.js
@@ -2,11 +2,34 @@ const ContentModel = require('../models/ContentModel');
 const espress = require('express');
 const router = express.Router();
 
-router.get('/why', (req, res) => {
+router.get('/why', async (req, res) => {
+    try {
+        const whyContent = await ContentModel.findOne({});
+        if (!whyContent) {
+            return res.status(404).json({ error: "Content not found" });
+        }
+        const whyIPECdescription = whyContent.whyIPEC.description;
+        const whyIPECsubTexts = whyContent.whyIPEC.subTexts;
+
+        res.json({
+            description: whyIPECdescription,
+            subTexts: whyIPECsubTexts
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "An error occoured" });
+    }
+});
+
+router.get('/about', async (req, res) => {
     try {
         
     }
-    catch {
-
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "An error occoured" });
     }
 });
+
+module.exports = router;
