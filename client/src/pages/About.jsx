4b1588862c9593e962412bc2e675e95608a9408a
17032000 <paarth17032000@gmail.com>
2023-07-21 18:48:47 +0530

    about, why and testimonials linked

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,196 +1,134 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../config";
 
 function About() {
+  const [userData, setUserData] = useState({})
+  // api/whyIPEC/get
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/aboutipec/get`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            auth: localStorage.getItem("token"),
+          },
+        }
+      );
+      console.log(response.data[0])
+      setUserData(response.data[0])
+      // setUserData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const advantageImgArr = [
+    "https://www.vidyamandir.com/assets/images/33.png",
+    "https://www.vidyamandir.com/assets/images/34.png",
+    "https://www.vidyamandir.com/assets/images/35.png",
+    "https://www.vidyamandir.com/assets/images/36.png"
+  ]
+
+  const pedagogyImgArr = [
+    "https://www.vidyamandir.com/assets/images/ped1.png",
+    "https://www.vidyamandir.com/assets/images/ped2.png",
+    "https://www.vidyamandir.com/assets/images/ped3.png",
+    "https://www.vidyamandir.com/assets/images/ped4.png"
+  ]
+
   return (
     <div className="bg-[#d1e9f9]">
-      
-    
-    <div className="max-w-6xl px-4 py-10 mx-auto ">
-      <div className="">
-        <div className="flex justify-center ">
-          <h2 className="text-3xl font-bold mb-4 900 border-b-[6px] border-yellow-400">
-            ABOUT IPEC 
-          </h2>
-        </div>
-        <p className="text-[16px] leading-relaxed text-center ">
-          Over the years, the name of Vidyamandir Classes (IPEC) has become
-          synonymous with success in IITJEE, NEET &amp; FOUNDATION. Our goal at
-          Vidyamandir Classes (IPEC) is to provide knowledge and guidance and
-          thereby create an environment that not only guides students to the
-          path of success but also inspires them to recognize and explore their
-          own potential.{" "}
-          <p className="mt-2 font-semibold">
-            The fact that more than 1,00,000 students appear every year for the
-            IPEC Test for the limited seats on offer is ample testimony to IPEC's
-            popularity and credence within the IITJEE, NEET &amp; FOUNDATION
-            aspirants' community.
-          </p>{" "}
-          All Courses are based on the time-tested teaching methodology, which
-          has been perfected by IPEC and has produced unmatched results in IITJEE
-          consistently over the past 35 years.
-        </p>
-      </div>
-
-      {/* */}
-
-      <div className="py-10 text-center">
-        <div className="container px-4 mx-auto">
+      {userData.AboutIPEC && (
+        <div className="max-w-6xl px-4 py-10 mx-auto ">
+        <div className="">
           <div className="flex justify-center ">
-            <h2 className="text-3xl font-bold mb-4 border-b-[6px] border-yellow-400">
-              IPEC ADVANTAGE
+            <h2 className="text-3xl font-bold mb-4 900 border-b-[6px] border-yellow-400">
+              ABOUT IPEC 
             </h2>
           </div>
-          <p className="text-lg leading-relaxed text-gray-700 ">
-            Studying at IPEC has its own set of advantages. Students are in a
-            better position to clear the competitive exams and be more. We help
-            students realize their fullest potential.
+          <p className="text-[16px] leading-relaxed text-center ">
+            {userData.AboutIPEC}
           </p>
-          <div className="flex flex-wrap mt-8 -mx-2">
-            <div className="w-full px-2 mb-4 sm:w-1/2 md:w-1/4 ">
-              <div className="bg-white rounded-lg shadow-lg p-6 h-[440px]">
-                <div className="text-center">
-                  <span>
-                    <img
-                      src="https://www.vidyamandir.com/assets/images/33.png"
-                      alt="Detailed Theory Portions"
-                    />
-                  </span>
-                  <h3 className="mt-4 mb-2 text-xl font-semibold">
-                    Detailed Theory Portions
-                  </h3>
-                </div>
-                <p className="leading-relaxed text-gray-700">
-                  Core concepts explained in a simple and easy-to-understand
-                  manner
-                </p>
-              </div>
-            </div>
-            <div className="w-full px-2 mb-4 sm:w-1/2 md:w-1/4 ">
-              <div className="bg-white rounded-lg shadow-lg p-6 h-[440px]">
-                <div className="text-center">
-                  <span>
-                    <img
-                      src="https://www.vidyamandir.com/assets/images/34.png"
-                      alt="Solved Examples &amp; In-Chapter Exercises"
-                    />
-                  </span>
-                  <h3 className="mt-4 mb-2 text-xl font-semibold">
-                    Solved Examples &amp; In-Chapter Exercises
-                  </h3>
-                </div>
-                <p className="leading-relaxed text-gray-700">
-                  Accurate and thorough explanations to ensure students
-                  internalize core concepts
-                </p>
-              </div>
-            </div>
-            <div className="w-full px-2 mb-4 sm:w-1/2 md:w-1/4 ">
-              <div className="bg-white rounded-lg shadow-lg p-6 h-[440px]">
-                <div className="text-center">
-                  <span>
-                    <img
-                      src="https://www.vidyamandir.com/assets/images/35.png"
-                      alt="Objective Worksheets"
-                    />
-                  </span>
-                  <h3 className="mt-4 mb-2 text-xl font-semibold">
-                    Objective Worksheets
-                  </h3>
-                </div>
-                <p className="leading-relaxed text-gray-700">
-                  A comprehensive set of question banks for every topic
-                </p>
-              </div>
-            </div>
-            <div className="w-full px-2 mb-4 sm:w-1/2 md:w-1/4 ">
-              <div className="bg-white rounded-lg shadow-lg p-6 h-[440px]">
-                <div className="text-center">
-                  <span>
-                    <img
-                      src="https://www.vidyamandir.com/assets/images/36.png"
-                      alt="Extensive Testing &amp; Evaluation"
-                    />
-                  </span>
-                  <h3 className="mt-4 mb-2 text-xl font-semibold">
-                    Extensive Testing &amp; Evaluation
-                  </h3>
-                </div>
-                <p className="leading-relaxed text-gray-700">
-                  These provide a simulated exam experience to students to
-                  prepare them for the pressures of IIT-JEE/NEET entrances
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
 
-      {/* */}
+        {/* */}
 
-      <div className="py-10 text-center">
-        <div className="container px-4 mx-auto">
-          <div>
+        <div className="py-10 text-center">
+          <div className="container px-4 mx-auto">
             <div className="flex justify-center ">
               <h2 className="text-3xl font-bold mb-4 border-b-[6px] border-yellow-400">
-                IPEC Pedagogy
+                IPEC ADVANTAGE
               </h2>
             </div>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
-              <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
-                <img
-                  src="https://www.vidyamandir.com/assets/images/ped1.png"
-                  alt="Detailed Theory Portions"
-                  className="w-24 mx-auto mb-4"
-                />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Concept Lectures &amp; Study Material
-                </h3>
-                <p className="leading-relaxed text-gray-700">
-                  Attend Concept Lectures &amp; Access Curated Study Material
-                </p>
+            <p className="text-lg leading-relaxed text-gray-700 ">
+              Studying at IPEC has its own set of advantages. Students are in a
+              better position to clear the competitive exams and be more. We help
+              students realize their fullest potential.
+            </p>
+            <div className="flex flex-wrap mt-8 -mx-2">
+              {userData.ipecAdvantages.map((advantage, index) => (
+                <div key={index} className="w-full px-2 mb-4 sm:w-1/2 md:w-1/4 ">
+                  <div className="bg-white rounded-lg shadow-lg p-6 h-[440px]">
+                    <div className="text-center">
+                      <span>
+                        <img
+                          src={advantageImgArr[index]}
+                          alt="Detailed Theory Portions"
+                        />
+                      </span>
+                      <h3 className="mt-4 mb-2 text-xl font-semibold">
+                        {advantage.title}
+                      </h3>
+                    </div>
+                    <p className="leading-relaxed text-gray-700">
+                    {advantage.description}
+                    </p>
+                  </div>
+                </div>
+              ))} 
+            </div>
+          </div>
+        </div>
+
+        {/* */}
+
+        <div className="py-10 text-center">
+          <div className="container px-4 mx-auto">
+            <div>
+              <div className="flex justify-center ">
+                <h2 className="text-3xl font-bold mb-4 border-b-[6px] border-yellow-400">
+                  IPEC Pedagogy
+                </h2>
               </div>
-              <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
-                <img
-                  src="https://www.vidyamandir.com/assets/images/ped2.png"
-                  alt="Solved Examples &amp; In-Chapter Exercises"
-                  className="w-24 mx-auto mb-4"
-                />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Live Classes &amp; Doubt Clearing
-                </h3>
-                <p className="leading-relaxed text-gray-700">
-                  Study Real Life Application &amp; Clear Doubts
-                </p>
-              </div>
-              <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
-                <img
-                  src="https://www.vidyamandir.com/assets/images/ped3.png"
-                  alt="Objective Worksheets"
-                  className="w-24 mx-auto mb-4"
-                />
-                <h3 className="mb-2 text-xl font-semibold">Online Tests</h3>
-                <p className="leading-relaxed text-gray-700">
-                  Test Your Concepts Using Curated Sample Papers
-                </p>
-              </div>
-              <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
-                <img
-                  src="https://www.vidyamandir.com/assets/images/ped4.png"
-                  alt="Extensive Testing &amp; Evaluation"
-                  className="w-24 mx-auto mb-4"
-                />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Performance Dashboard
-                </h3>
-                <p className="leading-relaxed text-gray-700">
-                  Evaluate Your Performances With In-Depth Reports
-                </p>
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+                {userData.ipecPedagogy.map((pedagogy, index) => (
+                  <div key={index} className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
+                    <img
+                      src={pedagogyImgArr[index]}
+                      alt="Detailed Theory Portions"
+                      className="w-24 mx-auto mb-4"
+                    />
+                    <h3 className="mb-2 text-xl font-semibold">
+                    {pedagogy.title}
+                    </h3>
+                    <p className="leading-relaxed text-gray-700">
+                    {pedagogy.description}
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      )}
     </div>
   );
 }
