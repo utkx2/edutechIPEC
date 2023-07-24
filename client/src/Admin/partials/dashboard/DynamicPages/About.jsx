d94a22976ea2312257d2b0b4d0c8a60b7718d0c5
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-07-24 20:14:27 +0530

    about page dynamic

--- client/src/Admin/partials/dashboard/DynamicPages/About.jsx
+++ client/src/Admin/partials/dashboard/DynamicPages/About.jsx
@@ -1,159 +1,30 @@
-import React from "react";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../../Sidebar";
 import Header from "../../Header";
 import { BASE_URL } from "../../../../config";
 import axios from "axios";
 
 export default function About() {
-  const [aboutIPEC, setAboutIPEC] = useState();
-  const [userData, setUserData] = useState([]);
-
-  const [ipecAdvantages, setIpecAdvantages] = useState({
-    title1: "",
-    title2: "",
-    title3: "",
-    title4: "",
-    desc1: "",
-    desc2: "",
-    desc3: "",
-    desc4: "",
-  });
-
-  const [ipecPedagogy, setIpecPedagogy] = useState({
-    title1: "",
-    title2: "",
-    title3: "",
-    title4: "",
-    desc1: "",
-    desc2: "",
-    desc3: "",
-    desc4: "",
-  });
-
-  const handleChangePedagogy = (e) => {
-    const { name, value } = e.target;
-    setIpecPedagogy((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleChangeAdvantage = (e) => {
-    const { name, value } = e.target;
-    setIpecAdvantages((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(ipecAdvantages, ipecPedagogy, aboutIPEC);
-    const ipecAdvantagesArr = [
-      {
-        title: ipecAdvantages.title1,
-        description: ipecAdvantages.desc1,
-      },
-      {
-        title: ipecAdvantages.title2,
-        description: ipecAdvantages.desc2,
-      },
-      {
-        title: ipecAdvantages.title3,
-        description: ipecAdvantages.desc4,
-      },
-      {
-        title: ipecAdvantages.title4,
-        description: ipecAdvantages.desc4,
-      },
-    ];
-
-    const ipecPedagogyArr = [
-      {
-        title: ipecPedagogy.title1,
-        description: ipecPedagogy.desc1,
-      },
-      {
-        title: ipecPedagogy.title2,
-        description: ipecPedagogy.desc2,
-      },
-      {
-        title: ipecPedagogy.title3,
-        description: ipecPedagogy.desc4,
-      },
-      {
-        title: ipecPedagogy.title4,
-        description: ipecPedagogy.desc4,
-      },
-    ];
-
-    const formData = {
-      AboutIPEC: aboutIPEC,
-      ipecAdvantages: ipecAdvantagesArr,
-      ipecPedagogy: ipecPedagogyArr,
-    };
-
-    const token = localStorage.getItem("token");
-
-    const requestBody = JSON.stringify(formData);
-
-    fetch(`${BASE_URL}AboutIpec/edit`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        auth: token,
-      },
-      body: requestBody,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("success", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("Oops something went wrong!!!");
-      });
-  };
+  const [aboutIPEC, setAboutIPEC] = useState("");
+  const [ipecAdvantages, setIpecAdvantages] = useState([]);
+  const [ipecPedagogy, setIpecPedagogy] = useState([]);
+  const [userData, setUserData] = useState({});
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  // {
-  //     "AboutIPEC": "IPEC is a leading educational institution...",
-  //         "ipecAdvantages": [
-  // {
-  //     "title": "",
-  //     "description": ""
-  // },
-  //             {
-  //                 "title": "State-of-the-Art Infrastructure",
-  //                 "description": "We provide modern classrooms and labs..."
-  //             }
-  //         ],
-  //             "ipecPedagogy": [
-  //                 {
-  //                     "title": "Interactive Learning",
-  //                     "description": "We emphasize interactive learning methodologies..."
-  //                 },
-  //                 {
-  //                     "title": "Project-Based Learning",
-  //                     "description": "Students work on real-world projects..."
-  //                 }
-  //             ]
-  // }
 
-  // api/whyIPEC/get
   const fetchData = async () => {
     try {
       const response = await axios.get(`${BASE_URL}aboutipec/get`, {
-        method: "GET",
         headers: {
           "Content-Type": "application/json",
           auth: localStorage.getItem("token"),
         },
       });
-      console.log("about", response.data[0]);
-      setUserData(response.data[0]);
-      // setUserData(response.data);
+      const data = response.data[0] || {};
+
+      setUserData(data);
+      setAboutIPEC(data.AboutIPEC || "");
+      setIpecAdvantages(data.ipecAdvantages || []);
+      setIpecPedagogy(data.ipecPedagogy || []);
     } catch (error) {
       console.error(error);
     }
@@ -163,10 +34,52 @@ export default function About() {
     fetchData();
   }, []);
 
-  return (
-    <div className="flex h-screen overflow-hidden ">
-      {/* Sidebar */}
+  const handleChangeAdvantage = (index, e) => {
+    const { name, value } = e.target;
+    setIpecAdvantages((prevAdvantages) =>
+      prevAdvantages.map((advantage, i) =>
+        i === index ? { ...advantage, [name]: value } : advantage
+      )
+    );
+  };
 
+  const handleChangePedagogy = (index, e) => {
+    const { name, value } = e.target;
+    setIpecPedagogy((prevPedagogy) =>
+      prevPedagogy.map((pedagogy, i) =>
+        i === index ? { ...pedagogy, [name]: value } : pedagogy
+      )
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    const formData = {
+      AboutIPEC: aboutIPEC,
+      ipecAdvantages: ipecAdvantages,
+      ipecPedagogy: ipecPedagogy,
+    };
+
+    axios
+      .put(`${BASE_URL}AboutIpec/edit`, formData, {
+        headers: {
+          "Content-Type": "application/json",
+          auth: token,
+        },
+      })
+      .then((response) => {
+        console.log("success", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Oops something went wrong!!!");
+      });
+  };
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */}
@@ -175,10 +88,9 @@ export default function About() {
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <div className="w-full px-4 py-8 mx-auto sm:px-6 lg:px-8 max-w-9xl">
             <div className="container p-6 mx-auto overflow-x-auto font-mono">
-              {/*---------> Table (Top Channels) */}
-
+              {/* Form */}
               <h1 className="mb-4 text-2xl font-bold">About</h1>
-              <div className="max-w-3xl px-4 py-8 mt-6 mb-6 rounded-lg shadow-xl  border-[2px] border-black">
+              <div className="max-w-3xl px-4 py-8 mt-6 mb-6 rounded-lg shadow-xl border-[2px] border-black">
                 <form onSubmit={handleSubmit}>
                   {/* Global Section */}
                   <div className="p-2 rounded-lg">
@@ -187,8 +99,7 @@ export default function About() {
                     </h1>
                     <textarea
                       type="text"
-                      name="title"
-                      value={userData.AboutIPEC}
+                      value={aboutIPEC}
                       onChange={(e) => setAboutIPEC(e.target.value)}
                       className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
                       placeholder="Enter About Desc"
@@ -196,71 +107,71 @@ export default function About() {
                     />
                   </div>
 
+                  {/* Advantages Section */}
                   <div className="p-2 rounded-lg">
                     <h1 className="mb-4 text-xl font-bold">
                       IPEC Advantaged Card Details
                     </h1>
                     <div className="flex flex-col gap-y-1">
-                      {userData.ipecAdvantages &&
-                        userData.ipecAdvantages.map((advantage, index) => (
-                          <div
-                            key={index}
-                            className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
-                          >
-                            <input
-                              type="text"
-                              name="title"
-                              value={advantage.title}
-                              onChange={handleChangeAdvantage}
-                              className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                              placeholder="Title1"
-                              required
-                            />
-                            <input
-                              type="text"
-                              name="description"
-                              value={advantage.description}
-                              onChange={handleChangeAdvantage}
-                              className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                              placeholder="Desc1"
-                              required
-                            />
-                          </div>
-                        ))}
+                      {ipecAdvantages.map((advantage, index) => (
+                        <div
+                          key={index}
+                          className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
+                        >
+                          <input
+                            type="text"
+                            name="title"
+                            value={advantage.title}
+                            onChange={(e) => handleChangeAdvantage(index, e)}
+                            className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder={`Title${index + 1}`}
+                            required
+                          />
+                          <input
+                            type="text"
+                            name="description"
+                            value={advantage.description}
+                            onChange={(e) => handleChangeAdvantage(index, e)}
+                            className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder={`Desc${index + 1}`}
+                            required
+                          />
+                        </div>
+                      ))}
                     </div>
                   </div>
 
+                  {/* Pedagogy Section */}
                   <div className="p-2 mt-4 rounded-lg">
                     <h1 className="mb-4 text-xl font-bold">
                       IPEC Pedagogy Card Details
                     </h1>
                     <div className="flex flex-col gap-y-1">
-                      {userData.ipecPedagogy &&
-                        userData.ipecPedagogy.map((pedagogy, index) => (
-                          <div
-                            key={index}
-                            className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
-                          >
-                            <input
-                              type="text"
-                              name="title"
-                              value={pedagogy.title}
-                              onChange={handleChangePedagogy}
-                              className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                              placeholder="Title1"
-                              required
-                            />
-                            <input
-                              type="text"
-                              name="description"
-                              value={pedagogy.description}
-                              onChange={handleChangePedagogy}
-                              className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                              placeholder="Desc1"
-                              required
-                            />
-                          </div>
-                        ))}
+                      {ipecPedagogy.map((pedagogy, index) => (
+                        <div
+                          key={index}
+                          className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
+                        >
+                          <input
+                            type="text"
+                            name="title"
+                            value={pedagogy.title}
+                            onChange={(e) => handleChangePedagogy(index, e)}
+                            className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder={`Title${index + 1}`}
+                            required
+                          />
+                          <input
+                            type="text"
+                            name="description"
+                            value={pedagogy.description}
+                            onChange={(e) => handleChangePedagogy(index, e)}
+                            className="w-full px-3 py-2 mt-1 text-black bg-gray-100 border rounded-sm focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder={`Desc${index + 1}`}
+                            required
+                          />
+                        </div>
+                      ))}
                     </div>
                   </div>
 
