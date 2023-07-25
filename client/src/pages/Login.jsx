6b7b8ff0496d46350a4df058ce9046639fbab75e
utkarsh somvanshi <somvanshi2132001@gmail.com>
2023-07-25 23:59:53 +0530

    commit-utkx2-29

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -77,7 +77,7 @@ const Login = () => {
                                 <form onSubmit={handleLogin}>
                                     <div className="relative z-0 w-full mb-6 group">
                                         <input
-                                            type="text"
+                                            type="email"
                                             name="identifier"
                                             id="floating_email"
                                             value={formData.identifier}
@@ -89,6 +89,9 @@ const Login = () => {
                                         <label
                                             htmlFor="floating_email"
                                             className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            style={{
+                                                zIndex: 1
+                                            }}
                                         >
                                             Email or Mobile Number
                                         </label>
@@ -107,6 +110,9 @@ const Login = () => {
                                         <label
                                             htmlFor="floating_password"
                                             className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            style={{
+                                                zIndex: 1
+                                            }}
                                         >
                                             Password
                                         </label>
@@ -129,7 +135,7 @@ const Login = () => {
                                     )}
                                 </form>
                                 <div className="mt-10 text-center">
-                                    Don not have an account?{" "}
+                                    Do not have an account?{" "}
                                     <Link
                                         to="/signup"
                                         className="text-sm font-medium text-[#1f1d5a] hover:text-red-800"
