4eddf6d9eeb4d145784d1cf24b4ad7d21cb54092
anuragasawa20 <img_2021012@iiitm.ac.in>
2023-08-15 00:02:23 +0530

    added bulk uploading and programs image

--- server/models/ResultsModel.js
+++ server/models/ResultsModel.js
@@ -1,31 +1,37 @@
 const mongoose = require('mongoose');
 
-const studentSchema = new mongoose.Schema({
-    centreName: {
-        type: String,
-        required: true,
-    },
-    studentName: {
-        type: String,
-        required: true,
-    },
-    IPECRollNo: {
-        type: String,
-        required: true,
-    },
-    CRLRank: {
-        type: Number,
-        required: true,
-    },
+// const studentSchema = new mongoose.Schema({
+//     centreName: {
+//         type: String,
+//         required: true,
+//     },
+//     studentName: {
+//         type: String,
+//         required: true,
+//     },
+//     IPECRollNo: {
+//         type: String,
+//         required: true,
+//     },
+//     CRLRank: {
+//         type: Number,
+//         required: true,
+//     },
+// });
+
+const examSchema = new mongoose.Schema({
+    examName: String,
+    image: String,
 });
 
+
 const resultSchema = new mongoose.Schema({
-    examName: {
-        type: String,
-        required: true,
-    },
-    students: {
-        type: [studentSchema],
+    // examName: {
+    //     type: String,
+    //     required: true,
+    // },
+    exams: {
+        type: [examSchema],
         required: true,
     },
 });
